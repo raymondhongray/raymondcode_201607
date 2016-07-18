c8a8b822b620e906e5f3615c4a9d7e2733f1cd16
raymond.hung <raymond.hung>
2016-07-18 19:48:09 +0800

    layout half

--- tcbank_2.0/js/main.js
+++ tcbank_2.0/js/main.js
@@ -1,3 +1,20 @@
+function init() {
+    $('.container-01').css('height', $('.menu-bg').height() + 20);
+    $('.container-02').css('height', ($('.pad-bg').height() * 2) - 100);
+     $('.container-03').css('height', '700px');
+}
+
 $(document).ready(function() {
-  // var skrollr_obj = skrollr.init();
+
+    init();
+    var skrollr_obj = skrollr.init();
+
+});
+
+$(window).resize(function() {
+    init();
+});
+
+$(window).scroll(function() {
+	console.log($(window).scrollTop());
 });
